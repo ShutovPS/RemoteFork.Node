c00ec3fc2b89031cbccc69941daae26293a24edc
Pavel Shutov <shutov.ps@gmail.com>
2019-06-28 16:30:16 +0300

    Исправлена работа с файлом settings.json

--- settings-manager.js
+++ settings-manager.js
@@ -1,13 +1,28 @@
 "use strict";
 
 const fs = require("fs");
-const settingsPath = "settings.json";
-const defaultSettingsPath ="settings-default.json";
+const settingsPath = "./settings.json";
+const defaultSettingsPath ="./settings-default.json";
 
 function checkSettings() {
+    if (fs.existsSync(settingsPath)) {
+        try {
+            let settings = require(settingsPath);
+        } catch(errorLoad) {
+            console.error(errorLoad);
+
+            try {
+                fs.unlinkSync(settingsPath);
+            } catch(errorUnlink) {
+                console.error(errorUnlink);
+            }
+        }
+    }
+    
     if (!fs.existsSync(settingsPath)) {
         if (fs.existsSync(defaultSettingsPath)) {
-            fs.copyFileSync(defaultSettingsPath, settingsPath);
+            var buffer = fs.readFileSync(defaultSettingsPath);
+            fs.writeFileSync(settingsPath, buffer);
         }
     }
 }
